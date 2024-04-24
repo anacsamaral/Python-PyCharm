c47a3d2b60570dbb639bcca5f6c6756db91f8941
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-04-23 23:32:44 -0300

    troca de valores

---
+++ Ambientes de Programação-1/troca-valor.py
@@ -0,0 +1,11 @@
+va = int(input('Valor de vA: '))
+vb = int(input('Valor de vB: '))
+vc = int(input('Valor de vC: '))
+
+print('Varáveis originais: vA = %d, vB = %d e vC = %d' % (va, vb, vc))
+
+vA = vb
+vB = vc
+vC = va
+
+print('Variáveis trocadas: vA = %d, vB = %d e vC = %d' % (vA, vB, vC))
