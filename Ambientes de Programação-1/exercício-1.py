caef37fedfc7afaff77c34d1da81f6ea0dd3cd2e
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-04-23 23:21:57 -0300

    mercadoria

--- Ambientes de Programação-1/exercício-1.py
+++
@@ -1,9 +0,0 @@
-preco = float(input('Preço da mercadoria: '))
-percent = float(input('Percentual de desconto: '))
-
-descont = float(preco * (percent/100))
-valorF = float(preco - descont)
-
-print('Desconto de {:.2f} em uma mercadoria de {:.2f}' .format(descont, preco))
-print('Desconto: %.2f', %descont)
-print('Valor a pagar: %.2f', %valorF)
---
+++ Ambientes de Programação-1/mercadoria.py
@@ -0,0 +1,10 @@
+preco = float(input('Preço da mercadoria: '))
+desconto = int(input('Porcentagem de desconto: '))
+
+desconto = float(preco * (desconto/100))
+valor_final = float(preco - desconto)
+
+print('Desconto de R$ %.2f em uma mercadoria de R$ %.2f' % (desconto, preco))
+print('Valor do desconto: R$ %.2f' % desconto)
+print('Valor a pagar: R$ %.2f' % valor_final)
+
