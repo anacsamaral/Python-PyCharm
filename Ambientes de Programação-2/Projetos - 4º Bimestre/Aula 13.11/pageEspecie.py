3f101c5b4de8df63f08420a70003b81c6513afd0
Bowie <anacarolinesousadoamaral2020@gmail.com>
2024-11-13 21:58:17 -0300

    aplicando no navegador

--- Ambientes de Programação-2/Projetos - 4º Bimestre/Aula 13.11/pageEspecie.py
+++ Ambientes de Programação-2/Projetos - 4º Bimestre/Aula 13.11/pageEspecie.py
@@ -1,10 +1,30 @@
 import cherrypy
+from classes.Especie import Especie
 class PaginaEspecie():
     topo = open("html/cabecalho.html", encoding='utf-8').read()
     rodape = open("html/rodape.html", encoding='utf-8').read()
     @cherrypy.expose()
     def index(self):
         html = self.topo
-        html += '''<h2>colocar o formulário e uma tabela para exibir as espécies</h2>'''
+        html += self.montarTabela()
         html += self.rodape
+        return html
+
+    def montarTabela(self):
+        html = '''<table>
+        <tr>
+        <th>Código</th>
+        <th>Descrição</th>
+        <th>Ações</th>
+        </tr>'''
+    #buscar os dados do banco
+        objEspecie = Especie() #Objeto do tipo especie
+        dados = objEspecie.obterEspecie() #Será criado uma lista com o sql
+        for linha in dados:
+            html += '''<tr>
+                       <td> %s</td>
+                       <td> %s</td>
+                       <td> [Excluir] [Alterar]</td>
+                       </tr>''' %(linha['Esp_codigo'], linha['Esp_descricao'])
+        html += '''</table>''' #Fechar tabela
         return html
